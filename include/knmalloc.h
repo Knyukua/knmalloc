22c69b7d22c282e0150d0540065df9d198ad4ed5
Knyukua <74075239+Knyukua@users.noreply.github.com>
2023-01-24 18:40:45 +0300

    Added HUGE docs for knmalloc and knfree.

--- include/knmalloc.h
+++ include/knmalloc.h
@@ -3,8 +3,17 @@
 
 #include <stddef.h>
 
+/// @brief Allocates a memory chunk on the heap.
+/// @param size the size of the memory chunk to be allocated.
+/// @return On success returns pointer to the allocated memory chunk.
+///         Returns NULL if size parameter is 0 or there is no big enough available chunk of memory.
 void* knmalloc(size_t size);
+
+/// @brief Frees the allocated memory chunk pointed by the ptr.
+///        Does nothing if ptr is NULL or outside the "heap".
+/// @param ptr pointer to the allocated memory chunk.
 void knfree(void* ptr);
+
 void dumpAllocatedChunks();
 void dumpAvailableChunks();
 
